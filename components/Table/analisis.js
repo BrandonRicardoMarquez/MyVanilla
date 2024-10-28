d27d3dfb70d358cf5706557ec9bf0a769803870f
Brandon Ricardo Marquez Larios <ing.brandon.marquez@gmail.com>
2024-10-28 08:39:41 -0600

    Mi primer commit

---
+++ components/Table/analisis.js
@@ -0,0 +1,61 @@
+export function analisis (usera,makeAlert){
+    const table = document.createElement('table');
+    table.classList.add('w-full', 'text-sm', 'text-left', 'rtl:text-right', 'text-gray-500', 'dark:text-gray-400', 'shadow-md', 'sm:rounded-lg');
+
+    // Creo el thead
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+
+    const th1 = document.createElement('thead');
+    th1.textContent = 'Nombre';
+    th1.classList.add('px-4', 'py-3', 'text-xs', 'text-gray-700', 'uppercase', 'bg-gray-40', 'dark:bg-gray-500', 'dark:text-gray-300');
+
+    const th2 = document.createElement('th');
+    th2.textContent = 'Estatura';
+    th2.classList.add('px-4', 'py-3', 'text-xs', 'text-gray-700', 'uppercase', 'bg-gray-40', 'dark:bg-gray-500', 'dark:text-gray-300');
+    
+    const th3 = document.createElement('th');
+    th3.textContent = 'Peso';
+    th3.classList.add('px-4', 'py-3', 'text-xs', 'text-gray-700', 'uppercase', 'bg-gray-40', 'dark:bg-gray-500', 'dark:text-gray-300');
+
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    //crea tbody de la tabla
+    const tbody = document.createElement('tbody');
+    tbody.classList.add('bg-white', 'divide-y', 'divide-gray-200', 'dark:divide-gray-700');
+
+    usera.forEach(client => {
+        const tr = document.createElement('tr');
+        tr.classList.add('odd:bg-white', 'odd:dark:bg-gray-900', 'even:bg-gray-50', 'even:dark:bg-gray-800', 'border-b', 'dark:border-gray-700');
+
+        const td1 = document.createElement('td');
+        td1.textContent = client.name;
+        td1.classList.add('px-6', 'py-4', 'font-medium', 'text-gray-900', 'whitespace-nowrap', 'dark:text-white');
+
+        const td2 = document.createElement('td');
+        td2.textContent = client.estatura;
+        td2.classList.add('px-6', 'py-4');
+
+        const td3 = document.createElement('td');
+        const obtenerpeso = document.createElement('button');
+        obtenerpeso.textContent = 'peso';
+        obtenerpeso.onclick = () => {
+            makeAlert(client.peso);
+        }
+        obtenerpeso.classList.add('font-medium', 'text-blue-600', 'dark:text-blue-500', 'hover:underline');
+        td3.appendChild(obtenerpeso);
+        td3.classList.add('px-6', 'py-4');
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tbody.appendChild(tr);
+    }
+    );
+    table.appendChild(tbody);
+    return table;
+}
--- views/Home.js
+++ views/Home.js
@@ -3,6 +3,7 @@ import { UserCard } from '../components/UserCard.js';
 import { Table} from '../components/Table/Table.js';
 import { makeAlert } from '../components/Table/functions.js';
 import { Reporte } from '../components/Table/Reporte.js'; 
+import { analisis } from '../components/Table/analisis.js';
 
 export function Home() {
   const container = document.createElement('div');
@@ -24,20 +25,34 @@ export function Home() {
     { name: 'Lola Ruiz', age: 22 }
   ];
 
+  const usera = [
+    { name: 'Sage', estatura: 1.52 , peso: 241 },
+    { name: 'Usa', estatura: 1.57 , peso: 165 },
+    { name: 'Inukai', estatura: 1.65 ,  peso: 81 }
+  ];
+
   // Agregar cada tarjeta de usuario
   const cardContainer = document.createElement('div');
   cardContainer.classList.add('grid', 'grid-cols-1', 'md:grid-cols-2', 'lg:grid-cols-3', 'gap-4');
+
   
   users.forEach(user => {
     const card = UserCard(user.name, user.age);
     cardContainer.appendChild(card);
   });
 
+  // usera.forEach (cliente => {
+  //   const card = UserCard(cliente.name, cliente.estatura, cliente.peso );
+  //   cardContainer.appendChild(card);
+  // });
+
   container.appendChild(cardContainer);
 
   container.appendChild(Table(users, true, makeAlert));
   container.appendChild(Table(otherUsers, true, makeAlert));
   cardContainer.appendChild(Reporte(users , true, makeAlert));
+  cardContainer.appendChild(analisis(usera, makeAlert))
+
  
 
 
